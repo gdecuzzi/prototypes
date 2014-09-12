6bd08b79a73eb0a1b1e0253e990c876d57997cbf
Gisela <gdecuzzi@10pines.com>
2014-09-12 01:05:03 -0300

    Mas tests para propiedades

--- test/add_property_spec.rb
+++ test/add_property_spec.rb
@@ -3,10 +3,31 @@ require_relative '../src/prototyped_object'
 
 describe 'Add Property' do
 
-  it 'agregar una propiedad la inicializa y entiende el getter' do
+  it 'agregar una propiedad entiende el getter' do
+    guerrero = PrototypedObject.new
+    guerrero.set_property(:energia, 100)
+    guerrero.energia
+  end
+
+  it 'agregar una propiedad con valor la inicializa' do
     guerrero = PrototypedObject.new
     guerrero.set_property(:energia, 100)
     expect(guerrero.energia).to eq(100)
   end
+
+  it 'agregar una propiedad entiende el setter' do
+    guerrero = PrototypedObject.new
+    guerrero.set_property(:energia, 100)
+    guerrero.energia=50
+  end
+
+  it 'agregar una propiedad cambia su valor despues del setter' do
+    guerrero = PrototypedObject.new
+    guerrero.set_property(:energia, 100)
+    guerrero.energia=50
+    expect(guerrero.energia).to eq(50)
+  end
+
+
 
 end
