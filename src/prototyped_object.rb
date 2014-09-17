a11efdabb6330767c23f248888301bc9c2d28693
Gisela <gdecuzzi@10pines.com>
2014-09-17 00:32:16 -0300

    rolback de todo para tener el diccionario de metodos que no queria tener porque ruby tiene checkeos al final (y no se como mandar a ejecutar un metodo cambiandole el self sin que explote por el aire)

--- src/prototyped_object.rb
+++ src/prototyped_object.rb
@@ -1,13 +1,23 @@
-
 class PrototypedObject
+  def initialize
+    @allBehavior = Hash.new
+    super
+  end
 
   def set_property(name, value)
     self.singleton_class.send :attr_accessor, name
     self.send "#{name}=", value
+
+    @allBehavior.store name, proc { eval("@#{name}")}
+
+
+
   end
 
   def set_method(selector, code)
     self.singleton_class.send :define_method, selector, code
+
+    @allBehavior.store selector, code
   end
 
   def set_prototype(aPrototype)
@@ -15,22 +25,26 @@ class PrototypedObject
   end
 
   def method_missing(selector, *arguments, &block)
-    if(@parent != nil?)
-      if(arguments.empty?)
-        eval("@parent.#{selector}", binding)
-      else
-        @arg = arguments[0]
-        eval("@parent.#{selector} #{@arg}", binding)
-      end
-
-
-    else
-      super.method_missing selector, arguments
+    if(@parent == nil?)
+      return super.method_missing selector, arguments
     end
+
+    @code = @parent.obtain_proc selector
+    if(@code == nil)
+      return super.method_missing selector, arguments
+    end
+
+    self.instance_eval &@code
+
+
   end
 
   def respond_to_missing?(method_name, include_private = false)
     (@parent != nil && (@parent.respond_to? method_name))|| (super method_name, include_private)
+  end
+
+  def obtain_proc(selector)
+    @allBehavior[selector]
   end
 
 end
